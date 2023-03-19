1297fb3d770274d045b9602b751e74871017f40a
William Tanardi <william.tanardi-2021@fst.unair.ac.id>
2023-03-19 18:20:12 +0700

    fixed routing, finished landing

---
+++ js/script.js
@@ -0,0 +1,37 @@
+const kitchenBtn = document.querySelector("#kitchenBtn");
+const pantryBtn = document.querySelector("#pantryBtn");
+const recipeBtn = document.querySelector("#recipeBtn");
+
+const kitchenHero = document.querySelector("#kitchenHero");
+const pantryHero = document.querySelector("#pantryHero");
+const recipeHero = document.querySelector("#recipeHero");
+
+kitchenBtn.addEventListener("click", () => {
+  kitchenHero.style.display = "flex";
+  pantryHero.style.display = "none";
+  recipeHero.style.display = "none";
+
+  kitchenBtn.classList.add("active");
+  pantryBtn.classList.remove("active");
+  recipeBtn.classList.remove("active");
+});
+
+pantryBtn.addEventListener("click", () => {
+  kitchenHero.style.display = "none";
+  pantryHero.style.display = "flex";
+  recipeHero.style.display = "none";
+
+  kitchenBtn.classList.remove("active");
+  pantryBtn.classList.add("active");
+  recipeBtn.classList.remove("active");
+});
+
+recipeBtn.addEventListener("click", () => {
+  kitchenHero.style.display = "none";
+  pantryHero.style.display = "none";
+  recipeHero.style.display = "flex";
+
+  kitchenBtn.classList.remove("active");
+  pantryBtn.classList.remove("active");
+  recipeBtn.classList.add("active");
+});
